fddd4a90854532e4e764610931f5d19d01460f54
Declan Joseph <declan.obu@outlook.com>
2026-03-31 11:47:24 +0300

    update exported index packages

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export * as core from '@core';
 export * as iter from '@iter';
+export * as database from '@database';
+export * as fieldcolumns from '@fieldcolumns';
+export * as filter from '@filter';
+export * as flattener from '@flattener';
+export * as unflattener from '@unflattener';
